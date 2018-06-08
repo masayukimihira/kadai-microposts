264a8793464326da8e18339dfd7314f0280d8ec1
masayukimihira <ft.lvlv.dcn@gamil.com>
2018-06-08 03:22:31 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@
 */
 
 
+
 Route::get('/', function () {
     return view('welcome');
+    
     
 });
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -22,6 +24,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-Route::group(['middleware' => ['auth']], function () {
+Route::get('/', 'MicropostsController@index');
+
+    Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
+
+
